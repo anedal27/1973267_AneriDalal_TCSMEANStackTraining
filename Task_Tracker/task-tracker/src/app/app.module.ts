e617082c69b6195cf5a75adfe4009a4450282af8
Aneri Dalal <anku127@gmail.com>
2021-03-29 14:30:12 -0400

    Created Task Tracker project and added form and display components

---
+++ Task_Tracker/task-tracker/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { TaskFormComponent } from './task-form/task-form.component';
+import { DisplayTasksComponent } from './display-tasks/display-tasks.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TaskFormComponent,
+    DisplayTasksComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
